41b64ea2f7543e6ac474cf86e80790e32dd8a9a6
Kristina <kristina09091983@gmail.com>
2024-11-10 12:06:10 +0200

    Add files via upload

--- ProductTest.java
+++ ProductTest.java
@@ -1,36 +1,20 @@
 package qa.automation;
 
 import base.TestUtil;
-import com.opencsv.exceptions.CsvException;
-import org.testng.Assert;
-import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 import pages.LoginPage;
 import pages.ProductsPage;
-import utils.CsvHelper;
-
-import java.io.IOException;
-
+import static org.testng.AssertJUnit.assertEquals;
 
 public class ProductTest extends TestUtil {
 
     @Test
-    public void addItemToTheCart() {
+    public void addTwoProductsToCartTest() {
         LoginPage loginPage = new LoginPage(driver);
         ProductsPage productPage = loginPage.login("standard_user", "secret_sauce");
-        //productPage.addItemToTheCart("onesie");
-        String[] items = {"backpack", "bike-light"};
-        for (String item : items) {
-            productPage.addItemToTheCart(item);
-        }
-        int itemsInCart = productPage.getItemsInTheCart();
-        assert itemsInCart == items.length : "Items in cart mismatch!";
+        productPage.addFirstProductToCart();
+        productPage.addSecondProductToCart();
 
-
-
-        //Assert.assertEquals(productPage.getItemsInTheCart(), 1, "Because we have only one item to far");
-        }
-
-
+        assertEquals("The cart should contain 2 items.", 2,productPage.getCartItemCount());
+    }
 }
-
